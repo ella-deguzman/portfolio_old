c7f6d6205bd481c4c7f6b5a28461c55764eb13dc
elladeguzman <elladeguzmann@gmail.com>
2019-11-20 22:44:15 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Ella DeGuzman Period 2]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">Home</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="DeGuzman_AboutMe.html">About Me</a></li></br>
+      <li><a href="DeGuzman_Projects.html">Projects </a></li></br>
+      <li><a href="DeGuzman_Resume.html">Resume</a></li></br>
+      <li><a href="DeGuzman_WorkExperience.html">Work Experience</a></li></br>
+      <li><a href="DeGuzman_ContactMe.html">Contact Me</a></li></br>
+    <ul>
+  </body>
+</html>
